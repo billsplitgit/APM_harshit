b12a3aa5c5ea640e08817661fb9dce5a216bfe89
billsplitgit <billsplitgit@gmail.com>
2017-08-04 18:03:29 +0530

    6th commit. partial sprint4

---
+++ APM-Start-Updated/src/app/products/product.service.ts
@@ -0,0 +1,23 @@
+
+import {Injectable} from "@angular/core"
+import {Http, Response } from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/operator/map";
+
+import { IProduct } from "./product";
+
+@Injectable(
+
+)
+
+export class ProductService {
+    private _productUrl = 'api/prodicts/products.json'
+
+    constructor(private _http : Http){}
+
+    getProducts()  : Observable<IProduct[]> {
+        return this._http.get(this._productUrl)
+        .map((response: Response) => <IProduct[]> response.json());
+    }
+    
+}
