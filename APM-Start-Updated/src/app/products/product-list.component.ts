cb489f59e97c07f2dfea799bd1befef3a4c4e948
billsplitgit <billsplitgit@gmail.com>
2017-08-05 19:56:19 +0530

    7th commit. Partial sprint5

--- APM-Start-Updated/src/app/products/product-list.component.ts
+++ APM-Start-Updated/src/app/products/product-list.component.ts
@@ -5,7 +5,7 @@ import { ProductService } from "./product.service";
 
 
 @Component({
-    selector : 'pm-products',
+    //selector : 'pm-products',
     moduleId : module.id,
     templateUrl : 'product-list.component.html',
     styleUrls: ['product-list.component.css']  
